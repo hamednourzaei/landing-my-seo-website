6aa84e28d6e5f0ec6337ac958370a60aa07003b2
hamed <52428565+hamedmkm@users.noreply.github.com>
2025-05-29 15:33:12 +0330

    first ui

--- components/layout/sections/testimonial.tsx
+++ components/layout/sections/testimonial.tsx
@@ -106,72 +106,72 @@ export const TestimonialSection: React.FC = () => {
   };
 
   return (
-    <section id="success-stories" className="  container py-24 sm:py-32">
-      <hr className="border-secondary" />
-      <div className="text-center mb-8 py-24">
-        <h2 className="text-lg text-primary text-center mb-2 tracking-wider">
-          نظرات موفقیت آمیز مشتریان
-        </h2>
-        <h2 className="text-3xl md:text-4xl text-center font-bold mb-4">
-          TsarSEO موفقیت مشتریان
-        </h2>
-      </div>
+    <section id="success-stories" className="container py-24 sm:py-32">
+    <hr className="border-secondary mb-8" />
+    <div className="text-center mb-8 py-24">
+      <h2 className="text-lg text-primary text-center mb-2 tracking-wider">
+        نظرات موفقیت آمیز مشتریان
+      </h2>
+      <h2 className="text-3xl md:text-4xl text-center font-bold mb-4">
+        TsarSEO موفقیت مشتریان
+      </h2>
+    </div>
 
-      <Carousel
-        opts={{ align: "start" }}
-        className="relative w-[80%] sm:w-[90%] lg:max-w-screen-xl mx-auto"
-      >
-        <CarouselContent>
-          {storyList.map((story) => (
-            <CarouselItem key={story.id} className="md:basis-1/2 lg:basis-1/3">
-              <motion.div
-                className="h-full rounded-lg overflow-hidden"
-                initial={{ opacity: 0 }}
-                animate={isMobile ? animation : { opacity: 1 }}
-                whileHover={!isMobile ? animation : undefined}
-              >
-                <Card className="h-full bg-muted/50 dark:bg-card">
-                  <CardContent className="pt-6 pb-0">
-                    <div className="flex gap-1 pb-6">
-                      {Array.from({ length: Math.floor(story.rating) }).map(
-                        (_, i) => (
-                          <Star
-                            key={i}
-                            className="size-4 fill-primary text-primary"
-                          />
-                        )
-                      )}
-                      {story.rating % 1 !== 0 && (
-                        <Star className="size-4 text-primary" />
-                      )}
+    <Carousel
+      opts={{ align: "start" }}
+      className="relative w-[80%] sm:w-[90%] lg:max-w-screen-xl mx-auto"
+    >
+      <CarouselContent>
+        {storyList.map((story) => (
+          <CarouselItem key={story.id} className="md:basis-1/2 lg:basis-1/3">
+            <motion.div
+              className="h-full rounded-lg overflow-visible"
+              initial={{ opacity: 0 }}
+              animate={isMobile ? { ...animation, opacity: 1 } : { opacity: 1 }}
+              whileHover={!isMobile ? animation : undefined}
+            >
+              <Card className="h-full bg-muted/50 dark:bg-card">
+                <CardContent className="pt-6 pb-0">
+                  <div className="flex gap-1 pb-6">
+                    {Array.from({ length: Math.floor(story.rating) }).map(
+                      (_, i) => (
+                        <Star
+                          key={i}
+                          className="size-4 fill-primary text-primary"
+                        />
+                      )
+                    )}
+                    {story.rating % 1 !== 0 && (
+                      <Star className="size-4 text-primary" />
+                    )}
+                  </div>
+                  {`"${story.comment}"`}
+                </CardContent>
+                <CardHeader>
+                  <div className="flex flex-row items-center my-5 gap-4">
+                    <Avatar>
+                      <AvatarImage src={story.image} alt={story.name} />
+                      <AvatarFallback>
+                        {story.name
+                          .split(" ")
+                          .map((n) => n[0])
+                          .join("")}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col">
+                      <CardTitle className="text-lg">{story.name}</CardTitle>
+                      <CardDescription>{story.role}</CardDescription>
                     </div>
-                    {`"${story.comment}"`}
-                  </CardContent>
-                  <CardHeader>
-                    <div className="flex flex-row items-center my-5 gap-4">
-                      <Avatar>
-                        <AvatarImage src={story.image} alt={story.name} />
-                        <AvatarFallback>
-                          {story.name
-                            .split(" ")
-                            .map((n) => n[0])
-                            .join("")}
-                        </AvatarFallback>
-                      </Avatar>
-                      <div className="flex flex-col">
-                        <CardTitle className="text-lg">{story.name}</CardTitle>
-                        <CardDescription>{story.role}</CardDescription>
-                      </div>
-                    </div>
-                  </CardHeader>
-                </Card>
-              </motion.div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </section>
+                  </div>
+                </CardHeader>
+              </Card>
+            </motion.div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  </section>
   );
 };
