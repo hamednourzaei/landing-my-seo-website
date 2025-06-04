c666536d0ddb03705bd3dc8df1319d3b12ecc313
hamed <52428565+hamedmkm@users.noreply.github.com>
2025-06-04 14:21:16 +0330

    last commit 33900

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -51,12 +51,12 @@ const menuVariants = {
   closed: {
     y: "-100%",
     opacity: 0,
-    transition: { duration: 0.3, ease: "easeInOut" },
+    transition: { duration: 1.4, ease: "easeInOut" },
   },
   open: {
     y: 0,
     opacity: 1,
-    transition: { duration: 0.3, ease: "easeInOut" },
+    transition: { duration: 1.4, ease: "easeInOut" },
   },
 };
 
@@ -78,7 +78,7 @@ export const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <header className="shadow-inner font-kalameh font-extrabold bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
+    <header className="shadow-inner font-kalameh font-extrabold bg-opacity-90 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="font-bold text-lg flex items-center">
         <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
         TsarSEO
@@ -104,9 +104,9 @@ export const Navbar: React.FC = () => {
 
           <SheetContent
             side="top"
-            className="w-full h-full bg-gradient-to-b from-primary/10 to-card border-none rounded-b-2xl"
+            className="w-full h-full bg-gray-00/50 backdrop-blur-md border-none rounded-b-2xl z-50"
           >
-            <SheetHeader className="mb-6">
+            <SheetHeader className="mb-6 relative z-10">
               <SheetTitle className="flex items-center justify-center">
                 <Link href="/" className="flex items-center">
                   <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
@@ -116,7 +116,7 @@ export const Navbar: React.FC = () => {
             </SheetHeader>
 
             <motion.div
-              className="flex flex-col items-center gap-4"
+              className="flex flex-col items-center gap-4 relative z-10"
               variants={menuVariants}
               initial="closed"
               animate="open"
