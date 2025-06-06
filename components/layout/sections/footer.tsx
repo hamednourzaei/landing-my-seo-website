00d2e0d80771bf974a980cbc2d6d87148eb9b425
hamed <52428565+hamedmkm@users.noreply.github.com>
2025-06-06 22:35:04 +0330

    last commit 335kp

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export const FooterSection = () => {
   return (
-    <footer id="footer" className="container font-kalameh font-semibold py-24 sm:py-32">
-      <div className="p-10 bg-card border border-secondary rounded-2xl">
+    <footer id="footer" className="container font-kalameh font-semibold py-24 sm:py-32 ">
+      <div className="p-10 bg-card  rounded-2xl  border-[1px] border-orange-800 shadow-xl shadow-orange-400/10">
         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-sans font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
+              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border " />
               <h3 className="text-2xl font-normal">TsarSEO</h3>
             </Link>
           </div>
