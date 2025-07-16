160dabc7b081ba214f4f2adbc63d671296a97df3
hamed <52428565+hamednourzaei@users.noreply.github.com>
2025-07-16 16:12:35 +0330

    Update QueryProvider.tsx

--- components/layout/sections/QueryProvider.tsx
+++ components/layout/sections/QueryProvider.tsx
@@ -4,7 +4,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
-
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   // ایجاد QueryClient در سمت کلاینت
   const [queryClient] = useState(() => new QueryClient());
