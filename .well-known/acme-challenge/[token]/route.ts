423bbb7232cf789813886384fc187221180a58da
hamed <52428565+hamednourzaei@users.noreply.github.com>
2025-07-16 17:25:42 +0330

    Create route.ts

---
+++ .well-known/acme-challenge/[token]/route.ts
@@ -0,0 +1,30 @@
+// app/.well-known/acme-challenge/[token]/route.ts
+
+import { NextResponse } from 'next/server';
+
+const tokens: Record<string, string> = {
+  "ZS3WaHHPY1V0BqHGpxjlNdP2olZtkKRUI17b-iLwMcA":
+    "ZS3WaHHPY1V0BqHGpxjlNdP2olZtkKRUI17b-iLwMcA.-mrCzP1dsH9-M9NYVsTJU2K6I4BUXnZX7owXy2OJdVU",
+  "Q-ixUfdsoAJ0tns3HiIaKNd0AqhSVjp1yXjOtUjiHWA":
+    "Q-ixUfdsoAJ0tns3HiIaKNd0AqhSVjp1yXjOtUjiHWA.-mrCzP1dsH9-M9NYVsTJU2K6I4BUXnZX7owXy2OJdVU",
+};
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { token: string } }
+) {
+  const token = params.token;
+
+  const responseText = tokens[token];
+
+  if (!responseText) {
+    return new NextResponse('Token not found', { status: 404 });
+  }
+
+  return new NextResponse(responseText, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
